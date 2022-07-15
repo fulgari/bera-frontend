d6ae2c9ccee3494ca9d508bfa935e784039efaf7
Johnny Pan <swanf@outlook.com>
2022-07-16 00:29:05 +0800

    feat: tailwindcss cli & rename esbuild staff

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.tsx",
+    // './src/pages/**/*.{html,tsx}',
+    // './src/components/**/*.{html,tsx}',
+    // './src/layout/**/*.{html,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
