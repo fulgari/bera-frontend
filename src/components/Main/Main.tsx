2c10b8d83eb21cbfe4767babc7b2710dcd889356
Johnny Pan <swanf@outlook.com>
2022-11-29 17:01:28 +0800

    chore: update ts & loading icon

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -32,35 +32,7 @@ export default function (props: MainProps) {
   // if (error) return "An error has occurred: " + (error as any).message;
 
   return isLoading ? (
-    <div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-        style={{ margin: "auto", background: "#fff", display: "block" }}
-        width="200px"
-        height="200px"
-        viewBox="0 0 100 100"
-        preserveAspectRatio="xMidYMid"
-      >
-        <circle
-          cx="50"
-          cy="50"
-          fill="none"
-          stroke="#fee6e9"
-          stroke-width="10"
-          r="35"
-          stroke-dasharray="164.93361431346415 56.97787143782138"
-        >
-          <animateTransform
-            attributeName="transform"
-            type="rotate"
-            repeatCount="indefinite"
-            dur="1s"
-            values="0 50 50;360 50 50"
-            keyTimes="0;1"
-          ></animateTransform>
-        </circle>
-      </svg>
+    <div className={styles.dotsBars}>
     </div>
   ) : (
     // <div className={styles.entry}>
