bfd7b881e9cd0f8e4731fec42049466ce4e0080a
Johnny Pan <swanf@outlook.com>
2023-04-09 13:58:05 +0800

    feat: rm fe node cors

--- scripts/serve.koa.ts
+++ scripts/serve.koa.ts
@@ -1,10 +1,8 @@
 import Koa from 'koa';
 import koaStatic from 'koa-static';
-import cors from "@koa/cors";
 
 const koa = new Koa();
 
-koa.use(cors());
 koa.use(koaStatic(process.cwd() + '/public'))
 
 koa.listen(9004);
