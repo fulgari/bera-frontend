d00dd81c8850a5867e0b5ea255121cceb3e29edc
Johnny Pan <swanf@outlook.com>
2023-04-13 14:02:04 +0800

    feat: map btn

--- src/components/BasicHeader/BasicHeader.tsx
+++ src/components/BasicHeader/BasicHeader.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { minusDelta } from "../Main/MainSlice";
 import styles from "./BasicHeader.module.css";
+import cx from "classnames";
 
 type BasicHeaderProps = {};
 
@@ -15,14 +16,37 @@ export default function (props: BasicHeaderProps) {
   const year = date.toLocaleString("en-GB", { year: "numeric" });
   const month = date.toLocaleString("en-GB", { month: "short" });
 
+  const renderBtns = () => {
+    const btns = [
+      {
+        name: "Prev",
+        onClick: () => { dispatch({ type: "main/minusDelta", payload: {} }); }
+      },
+      {
+        name: "Cur",
+        onClick: () => { dispatch({ type: "main/resetDelta", payload: {} }); }
+      },
+      {
+        name: "Next",
+        onClick: () => { dispatch({ type: "main/addDelta", payload: {} }); }
+      },
+      {
+        name: "Setting",
+        onClick: () => { dispatch({ type: "", payload: {} }); }
+      }
+    ];
+    return btns.map((item, index) => {
+      return (
+        <div key={index} className={cx(styles.basicHeaderRightItem, "text-green-600")} onClick={item.onClick}>{item.name}</div>
+      )
+    })
+  }
+
   return (
     <div className={styles.basicHeaderWrap}>
       <div className={styles.basicHeaderLeft}>{year} {month}</div>
       <div className={styles.basicHeaderRight}>
-        <div className={styles.basicHeaderRightItem} onClick={() => { dispatch({ type: "main/minusDelta", payload: {} }); }}>Prev</div>
-        <div className={styles.basicHeaderRightItem} onClick={() => { dispatch({ type: "main/resetDelta", payload: {} }); }}>Cur</div>
-        <div className={styles.basicHeaderRightItem} onClick={() => { dispatch({ type: "main/addDelta", payload: {} }); }}>Next</div>
-        <div className={styles.basicHeaderRightItem}>Setting</div>
+        {renderBtns()}
       </div>
     </div>
   );
