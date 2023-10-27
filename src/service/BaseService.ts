7feff29604b19604f34883c5d97efd5b61e5f921
Johnny <swanf@outlook.com>
2023-10-27 22:49:47 +0800

    fix: empty response

--- src/service/BaseService.ts
+++ src/service/BaseService.ts
@@ -8,7 +8,7 @@ interface IReqParams {
 }
 
 axios.interceptors.response.use(response => response, error => {
-  if (error.response.status === 401) {
+  if (error?.response?.status === 401) {
     window.location.href = '/login'
   }
 })
